1f356a6cb338a72e2e9a12191fdafcf41a4da72f
Aníbal Alexandre <o.seu.email@exemplo.com>
2026-09-15 11:41:21 +0000

    Atualiza app e requirements corretos

---
+++ app.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Clube de Investimento APPO", page_icon="📈")
+st.title("Clube de Investimento APPO")
+st.write("Aplicação em execução via Streamlit.")
